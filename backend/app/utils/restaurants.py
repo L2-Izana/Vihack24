e409101c04292e3627dc3d3ebdf91a6f2a1f0be0
L2-Izana <dodinhluat6@gmail.com>
2024-08-18 11:19:14 +1000

    Updated metric calculation

--- backend/app/utils/restaurants.py
+++ backend/app/utils/restaurants.py
@@ -2,16 +2,16 @@ import numpy as np
 import math
 import requests
 from flask import jsonify
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from gensim.models import KeyedVectors
 from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
 
 # Base
 NUM_FEATURES = 4
-#categories include cuisine and diet type
 FEATURES = ['rating', 'distance-compatibility', 'budget-compatibility', 'categories-compatibility']
-
-MIN_RATINGS_THRESHOLD = 10  # Minimum number of ratings required to be considered (a threshold).
+MIN_RATINGS_THRESHOLD = 10 
 
 # Fetch from database
 BASE_COLLAB_FILTER_USER_W = np.repeat(1 / NUM_FEATURES, NUM_FEATURES)
@@ -19,18 +19,16 @@ BASE_COLLAB_FILTER_ITEM_W = np.repeat(1 / NUM_FEATURES, NUM_FEATURES)
 BASE_CONTENT_FILTERING_W = np.repeat(1 / NUM_FEATURES, NUM_FEATURES)
 
 # Urls
-# GOOGLE_MAPS_API_PLACE_DETAILS_URL = "https://maps.googleapis.com/maps/api/place/details/json" - old
 GOOGLE_MAPS_API_PLACE_URL = "https://places.googleapis.com/v1/places:searchNearby"
 
 # Private keys (need to put to .env later)
-# GOOGLE_MAPS_API_KEY = "AIzaSyBPq_817fag1tlgDk9u18ceM_lSbrJCx1Y"
 GOOGLE_MAPS_API_KEY = "AIzaSyDS3_F0Aer1Z2rq4jeqp7u3VCFrdqJLuFM"
 
 # Parameters that needs an algorithm later
 BASE_USER_BUDGET_LEVEL = 2.5
 
 #available categories
-valid_categories = [
+VALID_CATEGORIES = [
     'American', 'Italian', 'Vietnamese', 'Chinese', 'Mexican', 'Indian', 'Thai', 'French',
     'Japanese', 'Spanish', 'Mediterranean', 'Greek', 'Turkish', 'Korean', 'Lebanese',
     'Brazilian', 'Argentinian', 'Cuban', 'Caribbean', 'Persian', 'Moroccan', 'Ethiopian',
@@ -55,48 +53,36 @@ valid_categories = [
 model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 
 
-### FUNCTIONS DEFINITION
 
 def calculate_collective_weights(collab_user_w=BASE_COLLAB_FILTER_USER_W, col_item_w=BASE_COLLAB_FILTER_ITEM_W, content_w=BASE_CONTENT_FILTERING_W):
     collective_weights = (collab_user_w + col_item_w) * content_w
     return collective_weights.reshape(-1, 1)
 
 
-def calculate_restaurant_weighted_rating(fetched_restaurant_json, mean_rating):
-    rating = fetched_restaurant_json['rating']
-    rating_num = fetched_restaurant_json['user_ratings-total']
+def calculate_restaurant_weighted_rating(fetched_restaurant, mean_rating):
+    rating = fetched_restaurant['Rating']
+    rating_num = fetched_restaurant['User Ratings Total']
     weighted_rating = (rating * rating_num + mean_rating * MIN_RATINGS_THRESHOLD) / (rating_num + MIN_RATINGS_THRESHOLD)
     return weighted_rating
 
 
-def calculate_restaurants_weighted_rating(fetched_restaurants_json):
+def calculate_restaurants_weighted_rating(fetched_restaurants):
     try:
         scaler = MinMaxScaler()
         restaurants_weighted_rating = []
-        mean_rating = np.mean([fetched_restaurant['rating'] for fetched_restaurant in fetched_restaurants_json])
-        for fetched_restaurant_json in fetched_restaurants_json:
+        mean_rating = np.mean([fetched_restaurant['Rating'] for fetched_restaurant in fetched_restaurants])
+        for fetched_restaurant_json in fetched_restaurants:
             restaurant_weighted_rating = calculate_restaurant_weighted_rating(fetched_restaurant_json, mean_rating)
             restaurants_weighted_rating.append(restaurant_weighted_rating)
         restaurants_weighted_rating = np.array(restaurants_weighted_rating).reshape(-1,1)
         scaled_restaurants_weighted_rating = scaler.fit_transform(restaurants_weighted_rating)
-        # assert scaled_restaurants_weighted_rating.shape == (len(fetched_restaurants_json), 1)
+        # assert scaled_restaurants_weighted_rating.shape == (len(fetched_restaurants), 1)
         return scaled_restaurants_weighted_rating
     except Exception as e:
-        return np.repeat(0.5, len(fetched_restaurants_json)).reshape(-1, 1)
+        return np.repeat(0.5, len(fetched_restaurants)).reshape(-1, 1)
 
 
 def get_distance_from_place(fetched_restaurants, input_latitude, input_longitude):
-    # Fetch place details from Google Places API - old code
-    # url = f"{GOOGLE_MAPS_API_PLACE_DETAILS_URL}?place_id={place_id}&key={GOOGLE_MAPS_API_KEY}"
-    # response = requests.get(url)
-    # data = response.json()
-
-    # if 'result' in data and 'geometry' in data['result']:
-    #     place_latitude = data['result']['geometry']['location']['lat']
-    #     place_longitude = data['result']['geometry']['location']['lng']
-    # else:
-    #     raise ValueError("Error fetching place details or invalid Place ID")
-
     place_latitude = fetched_restaurants['Latitude']
     place_longitude = fetched_restaurants['Longitude']
 
@@ -117,26 +103,26 @@ def get_distance_from_place(fetched_restaurants, input_latitude, input_longitude
     return distance
 
 
-def calculate_restaurants_distance_compatibility(fetched_restaurants_json, current_location):
+def calculate_restaurants_distance_compatibility(fetched_restaurants, current_location):
     try:
         scaler = MinMaxScaler()
         restaurants_distance_compatibility = []
-        for fetched_restaurant_json in fetched_restaurants_json:
+        for fetched_restaurant_json in fetched_restaurants:
             restaurant_place_id = fetched_restaurant_json['place_id']
             current_location_latitude, current_location_longitude = current_location[0], current_location[1]
             restaurant_distance = get_distance_from_place(restaurant_place_id, current_location_latitude, current_location_longitude)
             restaurants_distance_compatibility.append(restaurant_distance)
         restaurants_distance_compatibility = np.array(restaurants_distance_compatibility).reshape(-1, 1)
         scaled_restaurants_distance_compatibility = scaler.fit_transform(restaurants_distance_compatibility)
-        # assert scaled_restaurants_distance_compatibility.shape == (len(fetched_restaurants_json), 1)
+        # assert scaled_restaurants_distance_compatibility.shape == (len(fetched_restaurants), 1)
         return scaled_restaurants_distance_compatibility
     except Exception as e:
-        return np.repeat(0.5, len(fetched_restaurants_json)).reshape(-1, 1)
+        return np.repeat(0.5, len(fetched_restaurants)).reshape(-1, 1)
 
 
-def calculate_restaurants_budget_compatibility(fetched_restaurants, input_user_budget=2.5):
+def calculate_restaurants_budget_compatibility(fetched_restaurants, input_user_budget=BASE_USER_BUDGET_LEVEL):
     try:
-        restaurant_price_level = [fetched_restaurant['price_level'] or 2.5 for fetched_restaurant in fetched_restaurants_json]
+        restaurant_price_level = [fetched_restaurant['Price Level'] or 2.5 for fetched_restaurant in fetched_restaurants]
         restaurant_price_level = np.array(restaurant_price_level)
         restaurants_price_level_compatibility = 1/np.exp(abs(restaurant_price_level - input_user_budget)/5)
         restaurants_price_level_compatibility = restaurants_price_level_compatibility.reshape(-1, 1)
@@ -154,9 +140,9 @@ def get_vector(model, word):
         return np.zeros((1, model.vector_size))  # Fallback to a zero vector or some default vector
     
  #input: user input categories (diet type, cuisines), output: compatibility with the current restaurant cuisine(s)
+
 def calculate_restaurants_categories_compatibility(fetched_restaurants, input_categories):
     input_vectors = []
-    # No cuisine data => 0
     if input_categories == 'N/A':
         return 0.0 
         
@@ -168,12 +154,10 @@ def calculate_restaurants_categories_compatibility(fetched_restaurants, input_ca
     else:
         return 0.0
     
-    #process current categories:
     fetched_restaurants['Categories'] = fetched_restaurants['Categories'].apply(lambda x: x.split(', ') if x != 'N/A' else [])
 
-    #Compute similarity scores 
+    # Compute similarity scores 
     scores = []
-
     for category_list in fetched_restaurants['Categories']:
         category_vectors = [get_vector(model, category) for category in category_list]
         
@@ -183,30 +167,29 @@ def calculate_restaurants_categories_compatibility(fetched_restaurants, input_ca
             scores.append(score)
         else:
             scores.append(0.0)
-
-
     scores = np.array(scores)
     scores = scores.reshape(-1, 1)
     return scores
 
-def calculate_restaurants_feature_metric(fetched_restaurants_json, current_location):
-    restaurants_weighted_rating = calculate_restaurants_weighted_rating(fetched_restaurants_json)
-    restaurants_distance_compatibility = calculate_restaurants_distance_compatibility(fetched_restaurants_json, current_location)
-    restaurants_budget_compatibility = calculate_restaurants_budget_compatibility(fetched_restaurants_json)
-    restaurants_food_type_compatibility = calculate_restaurants_food_type_compatibility(fetched_restaurants_json)
-    restaurants_food_cuisine_compatibility = calculate_restaurants_food_cuisine_compatibility(fetched_restaurants_json)
-    restaurants_feature_metric = np.hstack((restaurants_weighted_rating, restaurants_distance_compatibility, restaurants_budget_compatibility, restaurants_food_type_compatibility, restaurants_food_cuisine_compatibility))
+def calculate_restaurants_feature_metric(fetched_restaurants, current_location, input_categories):
+    restaurants_weighted_rating = calculate_restaurants_weighted_rating(fetched_restaurants)
+    restaurants_distance_compatibility = calculate_restaurants_distance_compatibility(fetched_restaurants, current_location)
+    restaurants_budget_compatibility = calculate_restaurants_budget_compatibility(fetched_restaurants)
+    restaurants_categories_compatibility = calculate_restaurants_categories_compatibility(fetched_restaurants, input_categories)
+    restaurants_feature_metric = np.hstack((restaurants_weighted_rating, restaurants_distance_compatibility, restaurants_budget_compatibility, restaurants_categories_compatibility))
     return restaurants_feature_metric
 
 
-def get_sorted_restaurants(fetched_restaurants, current_location):
-    restaurants_feature_metric = calculate_restaurants_feature_metric(fetched_restaurants, current_location)
+def get_sorted_restaurants(fetched_restaurants, current_location, input_categories):
+    restaurants_feature_metric = calculate_restaurants_feature_metric(fetched_restaurants, current_location, input_categories)
+
+    if isinstance(restaurants_feature_metric, pd.DataFrame):
+        restaurants_feature_metric = restaurants_feature_metric.to_numpy()
+
     collective_weights = calculate_collective_weights()
 
     restaurants_suitability = np.dot(restaurants_feature_metric, collective_weights)
 
-    # sorted_indices = np.argsort(restaurants_suitability.flatten())
-    # sorted_restaurants = [fetched_restaurants[i] for i in sorted_indices]
     fetched_restaurants_df = fetched_restaurants.copy()
 
     fetched_restaurants_df['Suitability'] = restaurants_suitability
@@ -270,7 +253,6 @@ def fetch_restaurants(api_key, latitude, longitude, radius=3000):
         return None
     
 def filter_categories(types):
-    # Handle the case where 'types' is empty or None
     if not types:
         return 'N/A'
     
@@ -278,23 +260,17 @@ def filter_categories(types):
     types_list = [t.strip() for t in types.split(',')]
     
     # Filter to keep only valid cuisines
-    filtered_list = [t for t in types_list if t in valid_categories]
+    filtered_list = [t for t in types_list if t in VALID_CATEGORIES]
     
     # If there are valid cuisines, return them joined; otherwise, return 'N/A'
     return ', '.join(filtered_list) if filtered_list else 'N/A'
 
 ## MAIN
-
 if __name__ == '__main__':
-    GOOGLE_MAPS_API_URL="https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    # GOOGLE_MAPS_API_URL="https://maps.googleapis.com/maps/api/place/nearbysearch/json"
 
     latitude = -37.8136
     longitude =  144.9631
-
-    # Fetch nearby restaurants
-    # url = f'{GOOGLE_MAPS_API_URL}?location={latitude},{longitude}&radius=1000&type=restaurant&key={GOOGLE_MAPS_API_KEY}'
-    # response = requests.get(url)
-    # response_data = response.json().get('results')
 
     #test input_categories
     input_categories = ['Vegan', 'Korean']
@@ -321,24 +297,14 @@ if __name__ == '__main__':
     data['Filtered_Restaurant_Types'] = data['Filtered_Restaurant_Types'].apply(
         lambda x: 'N/A' if not x or all(item.lower() == 'restaurant' for item in x.split(', ')) else x
     )
-
     data['Filtered_Restaurant_Types'] = data['Filtered_Restaurant_Types'].str.replace('Restaurant', '', case=False, regex=True).str.strip()
     data['Filtered_Restaurant_Types'] = data['Filtered_Restaurant_Types'].str.replace(r',\s*,', ', ', regex=True)  # Replace multiple commas
     data['Filtered_Restaurant_Types'] = data['Filtered_Restaurant_Types'].str.replace(r'^,\s*', '', regex=True)  # Remove leading comma
     data['Filtered_Restaurant_Types'] = data['Filtered_Restaurant_Types'].str.replace(r',\s*$', '', regex=True)  # Remove trailing comma
     data['Filtered_Restaurant_Types'] = data['Filtered_Restaurant_Types'].str.strip()  # Remove any remaining whitespace
-
     data['Categories'] = data['Filtered_Restaurant_Types'].apply(filter_categories)
-
     data = data[data['Categories'] != '']
-    #Price Levels: 
-    # - inexspensive: 1
-    # - moderate: 2
-    # - expensive: 3
-    # - very_expensive: 4
-
     data['Price Level'] = data['Price Level'].replace('N/A', np.nan)
-
     data['Price Level'] = data['Price Level'].replace({
         'PRICE_LEVEL_INEXPENSIVE': 1,
         'PRICE_LEVEL_MODERATE': 2,
@@ -346,15 +312,10 @@ if __name__ == '__main__':
         'PRICE_LEVEL_VERY_EXPENSIVE': 4
     })
 
-    data['Price Level'] = pd.to_numeric(data['Price Level'])
-
-    
-    #replace no price level with mean price level
+    data['Price Level'] = pd.to_numeric(data['Price Level'])    
     mean_price_level = data['Price Level'].mean()
     data.fillna({'Price Level': mean_price_level}, inplace=True)
 
-result = get_sorted_restaurants(data, (latitude, longitude), input_categories) #vegan, korean
+    result = get_sorted_restaurants(data, (latitude, longitude), input_categories) 
 
-print(result)
-
-
+    print(result)
